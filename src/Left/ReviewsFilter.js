cb6cbb43b8fe82a67a43c9e7cbca29071417ac79
Ayoubon <aarrasseayoub@gmail.com>
2023-05-27 18:16:33 +0100

    filter based on Ratings done

--- src/Left/ReviewsFilter.js
+++ src/Left/ReviewsFilter.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Left.css"; // Import the CSS file for styling
 import { BiCaretDown, BiCaretRight } from "react-icons/bi";
 import { AiFillStar } from "react-icons/ai";
+import ReviewList from "../review.json";
+import { useSelector, useDispatch } from "react-redux";
+import { rating } from "../Reducers/filter.js";
 function ReviewsFilter() {
   const [isExpanded, setIsExpanded] = useState(false);
-
+  const ratingValue = useSelector((state) => state.filter.rating);
+  const dispatch = useDispatch();
   const handleButtonClick = () => {
     setIsExpanded(!isExpanded);
   };
-
+  const RatingList = [5, 4, 3, 2, 1];
+  const NumberRating = reviewsRating(RatingList);
+  const maxV = Math.max(...NumberRating);
   return (
     <div>
       <button
@@ -40,116 +46,53 @@ function ReviewsFilter() {
           display: "grid",
         }}
       >
-        <div
-          style={{
-            display: "grid",
-            alignItems: "center",
-            gridTemplateColumns: "50% 30% 20%",
-          }}
-        >
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-          </div>
-          <div
-            style={{
-              backgroundColor: "#ccc",
-              width: "45px",
-              height: "15px",
-              borderRadius: "5px",
-              marginLeft: "5px",
-            }}
-          >
-            {" "}
-          </div>
-          <div style={{ justifySelf: "center", color: "gray" }}>136</div>
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="#ccc" />
-          </div>
-          <div
-            style={{
-              backgroundColor: "#ccc",
-              width: "30px",
-              height: "15px",
-              borderRadius: "5px",
-              marginLeft: "10px",
-            }}
-          >
-            {" "}
-          </div>
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="#ccc" />
-            <AiFillStar size={18} color="#ccc" />
-          </div>
-          <div
-            style={{
-              backgroundColor: "#ccc",
-              width: "30px",
-              height: "15px",
-              borderRadius: "5px",
-              marginLeft: "10px",
-            }}
-          >
-            {" "}
-          </div>
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="#ccc" />
-            <AiFillStar size={18} color="#ccc" />
-            <AiFillStar size={18} color="#ccc" />
-          </div>
-          <div
-            style={{
-              backgroundColor: "#ccc",
-              width: "30px",
-              height: "15px",
-              borderRadius: "5px",
-              marginLeft: "10px",
-            }}
-          >
-            {" "}
-          </div>
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <AiFillStar size={18} color="gold" />
-            <AiFillStar size={18} color="#ccc" />
-            <AiFillStar size={18} color="#ccc" />
-            <AiFillStar size={18} color="#ccc" />
-            <AiFillStar size={18} color="#ccc" />
-          </div>
-          <div
-            style={{
-              backgroundColor: "#ccc",
-              width: "30px",
-              height: "15px",
-              borderRadius: "5px",
-              marginLeft: "10px",
-            }}
-          >
-            {" "}
-          </div>
-        </div>
+        {RatingList.map((star) => {
+          return (
+            <div
+              style={{
+                display: "grid",
+                alignItems: "center",
+                gridTemplateColumns: "50% 30% 20%",
+              }}
+            >
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <AiFillStar size={18} color="gold" />
+                <AiFillStar size={18} color={star > 1 ? "gold" : "#ccc"} />
+                <AiFillStar size={18} color={star > 2 ? "gold" : "#ccc"} />
+                <AiFillStar size={18} color={star > 3 ? "gold" : "#ccc"} />
+                <AiFillStar size={18} color={star > 4 ? "gold" : "#ccc"} />
+              </div>
+              <div
+                style={{
+                  backgroundColor: "#ccc",
+                  width: 45 * (NumberRating[star - 1] / maxV),
+                  height: "15px",
+                  borderRadius: "5px",
+                  marginLeft: "5px",
+                }}
+              >
+                {" "}
+              </div>
+              <div style={{ justifySelf: "center", color: "gray" }}>
+                {NumberRating[star - 1]}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
+function reviewsRating(ratings) {
+  const results = [];
+  ratings.forEach((rating) => {
+    let counter = 0;
 
+    ReviewList.forEach((review) => {
+      parseInt(review.rating) === rating && counter++;
+    });
+    results[rating - 1] = counter;
+  });
+  return results;
+}
 export default ReviewsFilter;
